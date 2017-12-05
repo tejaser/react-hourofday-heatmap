754d3bb99ce9aae1129ba702a2e53f180aba929f
Tejas Pancholi <tejas.er@outlook.com>
2017-12-05 16:22:52 +0530

    cleaned up the files.

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const MILLISECONDS_IN_ONE_DAY = 24 * 60 * 60 * 1000;
+
+export const HOURS_IN_DAY = 24;
+
+export const DAY_LABELS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export const HOUR_LABELS = ["", "6", "", "12", "", "18", ""];
